bda65ee5bf829b67566b3a1d788b6d4238d1b21f
kxxheehxxn <fpqk7099@gmail.com>
2025-04-08 15:33:40 +0900

    wip: App.vue 수정 중

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import CalendarView from './views/CalendarView.vue'
 </script>
 
-<template></template>
+<template>
+  <CalendarView />
+</template>
 
 <style scoped></style>
