72f97ef77a8ed446f77edc5fa15ec9446e227175
kyuyueonyy <rbddl3470@naver.com>
2025-04-08 13:33:17 +0900

    header, footer

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from './HomeHeader.vue'
+import Footer from './HomeFooter.vue'
+</script>
+
+<template>
+  <div class="homepage">
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+.homepage {
+  background-color: var(--color-background);
+}
+</style>
