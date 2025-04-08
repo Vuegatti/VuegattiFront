c6c47e4413706bfd2e51c019c560eb50d2321b13
Ahnsaeyeon <saeyeon1515@g.skku.edu>
2025-04-08 15:26:43 +0900

    #3 feat: footer 작성

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 import HomeHeader from './components/HomeHeader.vue'
+import HomeFooter from './components/HomeFooter.vue'
 </script>
 
 <template>
   <HomeHeader />
+  <HomeFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <footer>
+    <span>Copyright 2025. Vuegatti. All Rights Reserved.</span>
+    <div class="linkContainer">
+      <i></i>
+      <a href="https://github.com/Vuegatti">https://github.com/Vuegatti</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  color: var(--color-text);
+  padding: var(--space-m);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+a {
+  color: var(--color-text);
+  /* text-decoration: none; */
+}
+
+i::after {
+  content: '';
+  background-image: url('@/assets/image/github_logo.png');
+  background-size: cover;
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+}
+
+.linkContainer {
+  display: flex;
+  gap: var(--space-s);
+  align-items: center;
+}
+
+.linkContainer :hover {
+  cursor: pointer;
+}
+</style>
