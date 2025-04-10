0745a24fc14910e68e1caa54fd44474639d5de47
kyuyueonyy <rbddl3470@naver.com>
2025-04-11 02:29:20 +0900

    design:main 재수정

--- src/stores/history.js
+++ src/stores/history.js
@@ -18,8 +18,8 @@ export const useHistory = defineStore('historyStore', () => {
 
       const response = await apiClient.get(`/history`)
       history.value = response.data
-        .filter(h => h.userID === ID && h.date.startsWith(prefix))
-        .sort((a, b) => new Date(b.date) - new Date(a.date))
+      .filter(h => h.userID === ID.value && h.date.startsWith(prefix))
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
     } catch (err) {
       console.log('거래내역 로딩 에러: ', err)
     }
