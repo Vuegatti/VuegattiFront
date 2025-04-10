1dff96025af6a792b48869b84877178f8a8555b3
kyuyueonyy <rbddl3470@naver.com>
2025-04-10 18:19:23 +0900

    feat: 홈페이지

--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ import router from './router'
 
 import MainView from '@/views/MainView.vue'
 import LogInView from './views/LogInView.vue'
-import SignInView from './views/SignInView.vue'
-
+import SignInVue from './views/SignInVue.vue'
 const app = createApp(App)
 
 app.use(createPinia())
