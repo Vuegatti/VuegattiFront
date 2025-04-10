69dea4a5818073bf2ec9e536816e72f1c26f9c14
Ahnsaeyeon <saeyeon1515@g.skku.edu>
2025-04-10 21:17:24 +0900

    feature: 은행 계좌 연결 push

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 
-import MainView from '@/views/MainView.vue'
-import LogInView from './views/LogInView.vue'
-import SignInVue from './views/SignInVue.vue'
 const app = createApp(App)
 
 app.use(createPinia())
