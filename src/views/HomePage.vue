eb58310f4a484ab5eba87b79640f8bc20e1c16ae
Ahnsaeyeon <saeyeon1515@g.skku.edu>
2025-04-11 12:47:27 +0900

    chore: db 오류 수정

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -24,38 +24,37 @@ import UserProfile from './UserProfile.vue'
   align-items: center;
   justify-content: center;
 
-  height: 80vh;
+  height: 80vh; /* 헤더/푸터 각 10vh 기준 */
   width: 100vw;
 
-  overflow: hidden;
+  overflow: hidden; /* 스크롤 방지 */
   padding: 0;
   box-sizing: border-box;
   padding-top: 4vh;
 }
 .container {
   display: flex;
-  /* flex-wrap: wrap; */
   background-color: var(--color-background);
   color: #f8f4f2;
-  width: 90%;
-  max-height: 100%;
+  width: 90%; /*100%;*/
+  max-height: 100%; /*80vh*/
   padding: 0;
-  max-height: 80vh;
-  /* height: 100vh; */
-  /* overflow: hidden; */
+  overflow: hidden;
+  align-items: stretch;
 }
 
 /* 왼쪽 컬럼 */
 .left {
   flex: 1;
-  padding: 2rem;
+  padding: 1rem;
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 2vh 2vw;
-  height: 100%;
+  flex-direction: column; /*  세로 배치 */
+  justify-content: space-evenly; /* 위쪽부터 정렬 */
+  align-items: center; /* 가운데 정렬 (선택사항) */
+  padding: 0 2vw;
+  height: 60vh; /* 부모 height 채움 */
 }
+
 /* 오른쪽 컬럼 */
 .right {
   flex: 1;
@@ -67,7 +66,7 @@ import UserProfile from './UserProfile.vue'
   width: 100%;
   position: relative;
   right: 100px;
-  margin-bottom: 50px;
-  /* overflow: hidden; */
+  margin-bottom: 0;
+  justify-content: space-evenly;
 }
 </style>
